4a37060fae61c58228aefbab935d520280fa9c66
pPaletas <jcoloradotech@gmail.com>
2026-03-07 15:14:38 -0500

    OS checker added

--- MedSAM2/app/segmentacion_model.py
+++ MedSAM2/app/segmentacion_model.py
@@ -24,10 +24,12 @@ DAVIS_PALETTE = b"\x00\x00\x00\x80\x00\x00\x00\x80\x00\x80\x80\x00\x00\x00\x80\x
 OUTPUT_DIR = os.getcwd() + "\\output"
 FRAMES_OUTPUT = "frames"
 VIDEO_OUTPUT = "video"
-# MODEL_CONFIG = r"//workspace/Segmentacion-Modelo/MedSam2/MedSamRepo/sam2/configs/sam2.1_hiera_t512.yaml"
-MODEL_CONFIG = os.getcwd() + "/sam2/configs/sam2.1_hiera_t512.yaml"
-# MODEL_CHECKPOINT = r"//workspace/Segmentacion-Modelo/MedSam2/MedSamRepo/MedSAM2_latest.pt"
-MODEL_CHECKPOINT = os.getcwd() + "/checkpoints/MedSAM2_latest.pt"
+if os.name == 'nt':
+    MODEL_CONFIG = os.getcwd() + "/sam2/configs/sam2.1_hiera_t512.yaml"
+    MODEL_CHECKPOINT = os.getcwd() + "/checkpoints/MedSAM2_latest.pt"
+else:
+    MODEL_CONFIG = r"//workspace/Segmentacion-Modelo/MedSam2/MedSamRepo/sam2/configs/sam2.1_hiera_t512.yaml"
+    MODEL_CHECKPOINT = r"//workspace/Segmentacion-Modelo/MedSam2/MedSamRepo/MedSAM2_latest.pt"
 
 class PromptData(TypedDict):
     box: tuple[int, int, int, int]
